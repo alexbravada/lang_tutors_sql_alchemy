c4b8e5fa952af0c388114a427fcbce488fe189bf
Alex U <sc2alex@yandex.ru>
2021-04-29 13:57:30 +0300

    /all/ random from sql

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ all_teachers_sql = db.session.query(Teacher).all()
 
 # all_random_teachers = random.sample(all_teachers_sql, len(all_teachers_sql))
 
-all_random_teachers = db.session.query(Teacher).order_by(func.random()).all()
+
 
 
 # main page
@@ -109,8 +109,8 @@ def render_index():
 # show us page with all tutors
 @app.route('/all/')
 def all_page():
+    all_random_teachers = db.session.query(Teacher).order_by(func.random()).all()
     print(all_random_teachers)
-    #teachers_sorted_by_rating = sorted(teachers, key=lambda teacher: teacher['rating'], reverse=True)
     teachers_sorted_by_rating = sorted(all_teachers_sql, key=lambda teacher: teacher.rating, reverse=True)
     count_teachers = len(all_teachers_sql)
     return render_template('all.html',
@@ -118,7 +118,6 @@ def all_page():
                            all_random_teachers=all_random_teachers,
                            teachers_sorted_by_rating=teachers_sorted_by_rating,
                            )
-#teachers=teachers,
 
 # show us page with tutors, what depends on var /<goal>
 @app.route('/goals/<goal>/')
